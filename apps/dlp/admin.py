ed6166bb18185e0145954f5bd3ed214a865db5ab
Axel Díaz <diaz.axelio@gmail.com>
2024-12-20 16:38:14 -0300

    chore(dlp): improving DetectedMessage admin

--- apps/dlp/admin.py
+++ apps/dlp/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from apps.dlp.models import Pattern, DetectedMessage
 
+
+@admin.register(DetectedMessage)
+class DetectedMessageAdmin(admin.ModelAdmin):
+    list_display = ("content", "pattern", "created", "modified")
+    search_fields = (
+        "content",
+        "pattern__name",
+    )
+    ordering = ("-created",)
+    list_filter = ("pattern", "created")
+
+
 admin.site.register(Pattern)
-admin.site.register(DetectedMessage)
