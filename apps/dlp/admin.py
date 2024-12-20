77d87b6f07133b29dd1634a7ffcc104383be43aa
Axel Díaz <diaz.axelio@gmail.com>
2024-12-20 16:52:31 -0300

    chore(dlp): improving detected message admin

--- apps/dlp/admin.py
+++ apps/dlp/admin.py
@@ -1,17 +1,32 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from apps.dlp.models import Pattern, DetectedMessage
 
 
 @admin.register(DetectedMessage)
 class DetectedMessageAdmin(admin.ModelAdmin):
-    list_display = ("content", "pattern", "created", "modified")
+    list_display = ("content", "pattern_link", "created", "modified")
     search_fields = (
         "content",
         "pattern__name",
     )
     ordering = ("-created",)
+    list_per_page = 10
     list_filter = ("pattern", "created")
+
+    @admin.display(description="Pattern Link")
+    def pattern_link(self, obj):
+        """
+        Return a clickable link to the related Pattern object in the admin interface.
+        """
+        if obj.pattern:
+            return format_html(
+                '<a href="{}">{}</a>',
+                obj.pattern.get_admin_url(),
+                obj.pattern.name,
+            )
+        return "-"
 
 
 admin.site.register(Pattern)
--- apps/dlp/models.py
+++ apps/dlp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.urls import reverse
 from model_utils.models import UUIDModel, SoftDeletableModel, TimeStampedModel
 
 
 class Pattern(UUIDModel, SoftDeletableModel):
     name = models.CharField(max_length=100)
     regex = models.TextField()
+
+    def get_admin_url(self):
+        """
+        Return the URL to the admin page for this Pattern object.
+        """
+        return reverse("admin:dlp_pattern_change", args=[self.id])
 
     def __str__(self):
         return self.name
